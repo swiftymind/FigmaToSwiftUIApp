79c6589d1c6d98ae9f95dcd8db00aac00987f2e4
Swifty Mind <muthukumar@ddn.upes.ac.in>
2025-05-24 12:41:42 +0530

    initial Upload

---
+++ FigmaToSwiftUIApp/FigmaToSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaToSwiftUIAppApp.swift
+//  FigmaToSwiftUIApp
+//
+//  Created by kanagasabapathy on 24.05.25.
+//
+
+import SwiftUI
+
+@main
+struct FigmaToSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FigmaToSwiftUIAppTests/FigmaToSwiftUIAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaToSwiftUIAppTests.swift
+//  FigmaToSwiftUIAppTests
+//
+//  Created by kanagasabapathy on 24.05.25.
+//
+
+import Testing
+@testable import FigmaToSwiftUIApp
+
+struct FigmaToSwiftUIAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
